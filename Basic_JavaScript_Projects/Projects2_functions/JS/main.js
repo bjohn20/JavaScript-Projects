b84b71f1d922f07c83e838e32006cdc140e1f9ff
Brandon-J <johnson.c.brandon@gmail.com>
2023-11-04 23:47:04 -0400

    Added comments to my work

--- Basic_JavaScript_Projects/Projects2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Projects2_functions/JS/main.js
@@ -1,12 +1,14 @@
 function My_First_Function() {
-  var str1 = "This is the button text!";
-  var str2 = "This is the second button text!";
+  // Define function and give it a name
+  var str1 = "This is the button text!"; // define variable and assign it to a string
+  var str2 = "This is the second button text!"; // define variable and assign it to a second string
 
-  document.getElementById("Button_Text").innerHTML = str1 + " " + str2;
+  document.getElementById("Button_Text").innerHTML = str1 + " " + str2; // Concatenate two variables and attaching the value to the id        "Button_Text"
 }
 
 function My_Second_Function() {
-  var statement = "I am tired";
-  statement += " from working a lot!";
-  document.getElementById("Concatenate").innerHTML = statement;
+  //Defin the function and give it a name
+  var statement = "I am tired"; // define a variable and assign it a string
+  statement += " from working a lot!"; // Concatenate variable and add another string
+  document.getElementById("Concatenate").innerHTML = statement; // Attaching variable value to "Concatenate" id
 }
