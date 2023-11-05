d93312d73fe5ca140e8ecabc95aa06c1b69e0c4c
Brandon-J <johnson.c.brandon@gmail.com>
2023-11-05 11:36:36 -0500

    Added comments to the code

--- Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -1,51 +1,61 @@
 function myAddition() {
-  var addition = 5 + 4;
-  document.getElementById("Math").innerHTML = addition;
+  // Define Function
+  var addition = 5 + 4; // Assign variable an expression
+  document.getElementById("Math").innerHTML = addition; //Use the document.getElementById method to select the html element with a id of Math and store the value of the addition variable to the the display
 }
 
 function mySubtraction() {
-  var subtraction = 9 - 2;
-  document.getElementById("second").innerHTML = subtraction;
+  // Define the function
+  var subtraction = 9 - 2; // Assigned variable an expression
+  document.getElementById("second").innerHTML = subtraction; // Use the document.getElementById method to select the html element with a id of  z second and store the value of the subtraction variable to the display
 }
 
 function myMultiplication() {
-  var multiplication = 8 * 10;
-  document.getElementById("third").innerHTML = multiplication;
+  // Define the function
+  var multiplication = 8 * 10; // Assign variable an expression
+  document.getElementById("third").innerHTML = multiplication; // Use the document.getElementById method to select the html element with a id of third and store the value of the multiplication variable to the display
 }
 
 function myDivision() {
-  var division = 20 / 5;
-  document.getElementById("fourth").innerHTML = division;
+  // Define the function to be called
+  var division = 20 / 5; // Assign the variable division an expression
+  document.getElementById("fourth").innerHTML = division; // Use the document.getElementById method to select the html element with a id of fourth and store the value of the division variable to the display
 }
 
 function myMultiple() {
-  var operations = (2 * 5) / 8 - 10 + 23;
-  document.getElementById("fifth").innerHTML = operations;
+  // Define the function to be called
+  var operations = (2 * 5) / 8 - 10 + 23; // Assign the variable operations to an expression
+  document.getElementById("fifth").innerHTML = operations; // Use the document.getElementById method to select the html element with a id of fifthand store the value of the operations variable to the display
 }
 
 function myModulus() {
-  var remainder = 25 % 3;
-  document.getElementById("sixth").innerHTML = remainder;
+  // Define the function to be called
+  var remainder = 25 % 3; // Assign the variable remainder to an expression
+  document.getElementById("sixth").innerHTML = remainder; // Use the document.getElementById method to select the html element with a id of sixth and store the value of the remainder variable to the display
 }
 
 function myNegation() {
-  var x = 20;
-  document.getElementById("seventh").innerHTML = -x;
+  // Define the function
+  var x = 20; // Assign the variable x to the value
+  document.getElementById("seventh").innerHTML = -x; // Use the document.getElementById method to select the html element with a id of seventh and store the value of the remainder variable to the display
 }
 
 function myIncrement() {
-  var x = 20;
-  var y = 54;
-  x++;
-  y--;
-  document.getElementById("eighth").innerHTML = "X = " + x + " and Y = " + y;
+  // Define the function to be called
+  var x = 20; // Assign the variable x to the value
+  var y = 54; // Assign the variable y to the value
+  x++; // Increment the variable x
+  y--; // Decrement the variable y
+  document.getElementById("eighth").innerHTML = "X = " + x + " and Y = " + y; // Use the document.getElementById method to select the html element with a id of eigth and store the value of the x & y variables to the display
 }
 
 function myRandom() {
-  window.alert(Math.random() * 50);
+  // Define the function to be called
+  window.alert(Math.random() * 50); // This javascript statement performs the window.alert method which invokes the Math.random function which is multiplied by 50 and displays the number in an alert window
 }
 
 function myRound() {
-  var x = 75.83;
-  document.getElementById("tenth").innerHTML = Math.round(x);
+  // Define the function to be called
+  var x = 75.83; // Assign the variable x to the value
+  document.getElementById("tenth").innerHTML = Math.round(x); // Use the document.getElementById method to select the html element with a id of tenth and store the value of the variable x to the display
 }
