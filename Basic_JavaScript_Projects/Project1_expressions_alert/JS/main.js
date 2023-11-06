bcf8c909617088a84ba250c9da1ae4c6a78df431
Brandon-J <johnson.c.brandon@gmail.com>
2023-11-06 13:25:34 -0500

    made correction on commented code

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,21 +1,21 @@
-// window.alert("Hello, world!"); // calling the window.alert function to display Hello World string
-// document.write("Hello World!"); // calling document.write function to display Hello World string
+window.alert("Hello, world!"); // calling the window.alert function to display Hello World string
+document.write("Hello World!"); // calling document.write function to display Hello World string
 
-// var A = "This is a string"; // Set a variable to a string
-// document.write(A); // Called the variable using document.write
-// var myTeam = "Knicks"; // Set a variable to a string
-// window.alert(myTeam); called the variable using window.alert
-// document.write("Brandon's Team is the Knicks!"); Utilize document.write to display string
+var A = "This is a string"; // Set a variable to a string
+document.write(A); // Called the variable using document.write
+var myTeam = "Knicks"; // Set a variable to a string
+window.alert(myTeam); called the variable using window.alert
+document.write("Brandon's Team is the Knicks!"); Utilize document.write to display string
 
-// document.write("Knicks" + " are my " + "favorite team."); // Concatenate three strings
+document.write("Knicks" + " are my " + "favorite team."); // Concatenate three strings
 
-// var familyCars = "The Johnsons", // set multiple variables to strings
-//   Me = "Nissan",
-//   Dad = "Lincoln",
-//   Friend = "Hyundai";
-// document.write(Me); // Called Me variable
+var familyCars = "The Johnsons", // set multiple variables to strings
+  Me = "Nissan",
+  Dad = "Lincoln",
+  Friend = "Hyundai";
+document.write(Me); // Called Me variable
 
-// document.write(5 + 3);
+document.write(5 + 3);
 
 var Sent1 = "This is the beginning of the string"; // Attached variable to a string
 var Sent2 = " and this is the end of the string"; // Attached variable to a string
