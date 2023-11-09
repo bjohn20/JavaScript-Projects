6ed3a7cd110c1f01e08cff54e887eb28920e81a4
Brandon-J <johnson.c.brandon@gmail.com>
2023-11-09 15:16:23 -0500

    Added comments to code

--- Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,33 +1,40 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1; // Declare variable
+showSlides(slideIndex); // the showSlides function makes a call and and displays the slide at the index which the slideIndex represents
 
 // Next/Previos controls
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+  // Define the plusSlides function with a parameter of n
+  showSlides((slideIndex += n)); // showSlides function is called and increments the slideIndex by n
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides((slideIndex = n));
+  // Define the currentSlide function with a parameter of n
+  showSlides((slideIndex = n)); // The showSlides function is called and it assigns the slideIndex value to the n parameter
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  // Define the showSlides function
+  let i; // Declare i variable
+  let slides = document.getElementsByClassName("mySlides"); // Declare slides variable and set it to the element with a classname of "mySlides"
+  let dots = document.getElementsByClassName("dot"); // Declare variable dots and set it to the elements with a classname of "dot"
   if (n > slides.length) {
-    slideIndex = 1;
+    // if statement that loops through the slides array with a condition statement, as long as n is greather than slide array execute code below
+    slideIndex = 1; // Sets the slideIndex value to 1
   }
   if (n < 1) {
-    slideIndex = slides.length;
+    // if statement with a conditon of if n is less than 1 execute the code below
+    slideIndex = slides.length; // sets the slideIndex value to the slide.length
   }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    // for loop with a conditon statement that iterates through the slides array
+    slides[i].style.display = "none"; // Sets each element of the slides array display property to "none"
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    // for loop that iterates through the dots array
+    dots[i].className = dots[i].className.replace(" active", ""); // Sets each element of the dots array to replace their classname from " active" to blank
   }
 
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  slides[slideIndex - 1].style.display = "block"; // access the slides array at the slideIndex -1 and sets the style.display property to block to display the element
+  dots[slideIndex - 1].className += " active"; // Assigns the active class to the dot array at the index of the slide
 }
